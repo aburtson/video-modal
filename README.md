ecc7e2f8c6ca6ff85686beaa30d9e97706504863
ogalex <alexburtson@Alexs-MacBook-Pro.local>
2017-07-27 16:15:39 -0700

    made general video modal, both youtube and wistia options

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
-#YouTube video modal
+#Video modal
 
-This plugin adds video modal functionality using YouTube videos.
+This plugin adds video modal functionality using either YouTube or Wistia videos.
 
 ##Setup
 
 1. _Dependencies:_ verify that the jQuery is included in your project.
 2. Include **video-modal.js** in your JS directory.
 3. Include **_video-modal.scss** or **video-modal.css** in your stylesheets.
-4. Add the `data-video=""` attribute to the element you want to click to open the modal, with your YouTube video ID inside the `""`. Refer to `video-modal-example.html` for reference.
-5. Run gulp, and the YouTube video modal will be up and running.
+4. Add the `data-video=""` attribute to the element you want to click to open the modal, with your video type inside the `""`. Choose either `youtube` and `wistia`, depending on where your video is hosted. Refer to `video-modal-example.html` for reference.
+5. Add the `data-video-id=""` attribute to the same element, and enter the video ID inside the `""`. Refer to `video-modal-example.html` for reference.
+6. Run gulp, and the YouTube video modal will be up and running.
--- video-modal.js
+++ video-modal.js
@@ -9,13 +9,22 @@ function videoModal() {
     var $body = $('body');
 
     $('[data-video]').on('click', function(){
-        var videoAttr = $(this).attr('data-video');
+        var videoType = $(this).attr('data-video');
+        var videoID = $(this).attr('data-video-id');
+        var videoEmbed;
+
+        if (videoType == 'youtube') {
+            videoEmbed = '<iframe src="https://www.youtube.com/embed/' + videoID + '?enablejsapi=1&iv_load_policy=3&playerapiid=wrapper_component_21&disablekb=1&wmode=transparent&controls=1&playsinline=0&showinfo=0&modestbranding=1&rel=0&autoplay=1&loop=0&vq=auto" frameborder="0" allowfullscreen></iframe>';
+        }
+        if (videoType == 'wistia') {
+            videoEmbed = '<script src="//fast.wistia.com/embed/medias/'+ videoID +'.jsonp" async></script><script src="//fast.wistia.com/assets/external/E-v1.js" async></script><div class="wistia_responsive_padding" style="padding:56.25% 0 0 0;position:relative;"><div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;"><div class="wistia_embed wistia_async_'+ videoID +' seo=false videoFoam=true" style="height:100%;width:100%"></div></div></div>';
+        }
         var videoModal = $(
             '<div class="video-modal">' +
                 '<div class="video-modal__inner">' +
                     '<span class="video-modal__close"></span>' +
                     '<div class="video__wrapper">' +
-                        '<iframe src="https://www.youtube.com/embed/' + videoAttr + '?enablejsapi=1&iv_load_policy=3&playerapiid=wrapper_component_21&disablekb=1&wmode=transparent&controls=1&playsinline=0&showinfo=0&modestbranding=1&rel=0&autoplay=1&loop=0&vq=auto" frameborder="0" allowfullscreen>' + '</iframe>' +
+                        videoEmbed +
                     '</div>' +
                 '</div>' +
             '</div>'
@@ -28,11 +37,15 @@ function videoModal() {
     $(document).on('click', '.video-modal', function(){
         var $this = $(this);
         $body.removeClass('video-modal--active');
+        console.log('removed active class');
         setTimeout(function(){
             $this.remove();
         }, 500);
     });
-    $(document).on('click', '.video-close', function(){
+    $(document).on('click', '.video-modal__inner', function(e){
+        e.stopPropagation();
+    });
+    $(document).on('click', '.video-modal__close', function(){
         $body.removeClass('video-modal--active');
         setTimeout(function(){
             $('.video-modal').remove();
