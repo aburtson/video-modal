622d404e0fbad7d614c58fffcf2751c9b12385f5
ogalex <alexburtson@Alexs-MacBook-Pro.local>
2017-07-27 14:43:48 -0700

    cleanup, add readme

---
+++ README.md
@@ -0,0 +1,11 @@
+#YouTube video modal
+
+This plugin adds video modal functionality using YouTube videos.
+
+##Setup
+
+1. _Dependencies:_ verify that the jQuery is included in your project.
+2. Include **video-modal.js** in your JS directory.
+3. Include **_video-modal.scss** or **video-modal.css** in your stylesheets.
+4. Add the `data-video=""` attribute to the element you want to click to open the modal, with your YouTube video ID inside the `""`. Refer to `video-modal-example.html` for reference.
+5. Run gulp, and the YouTube video modal will be up and running.
--- video-modal.js
+++ video-modal.js
@@ -2,31 +2,41 @@
  **************************************************************************************/
 // must have a .site-constraint or body set to relative
 
+videoModal();
+
 function videoModal() {
     var $siteConstraint = $('.site-constraint');
     var $body = $('body');
 
     $('[data-video]').on('click', function(){
+        console.log('data-video click');
         var videoAttr = $(this).attr('data-video');
-        var videoModal = $('<div class="video-modal"><span class="video-close"></span><div class="video-wrapper"><div class="video-container"><iframe ' + videoAttr + '></iframe></div></div></div>');
+        var videoModal = $(
+            '<div class="video-modal">' +
+                '<div class="video-modal__inner">' +
+                    '<span class="video-modal__close"></span>' +
+                    '<div class="video__wrapper">' +
+                        '<iframe src="https://www.youtube.com/embed/' + videoAttr + '?enablejsapi=1&iv_load_policy=3&playerapiid=wrapper_component_21&disablekb=1&wmode=transparent&controls=1&playsinline=0&showinfo=0&modestbranding=1&rel=0&autoplay=1&loop=0&vq=auto" frameborder="0" allowfullscreen>' + '</iframe>' +
+                    '</div>' +
+                '</div>' +
+            '</div>'
+        );
         $siteConstraint.append(videoModal);
         setTimeout(function(){
-            $body.addClass('video-modal-active');
+            $body.addClass('video-modal--active');
         }, 200);
     });
     $(document).on('click', '.video-modal', function(){
         var $this = $(this);
-        $body.removeClass('video-modal-active');
+        $body.removeClass('video-modal--active');
         setTimeout(function(){
             $this.remove();
         }, 500);
-
     });
     $(document).on('click', '.video-close', function(){
-        $body.removeClass('video-modal-active');
+        $body.removeClass('video-modal--active');
         setTimeout(function(){
             $('.video-modal').remove();
         }, 500);
-
     });
 }
